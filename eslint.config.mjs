f2c28e193e18fe5e9a724fcd7d254e08f640d676
Raymond Hammarling <raymond@hammarlings.se>
2024-09-03 02:07:30 +0200

    Upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends('kellyirc'), {
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        ecmaVersion: 2022,
+        sourceType: 'commonjs',
+    },
+
+    rules: {
+        'require-yield': 'off',
+
+        'space-before-function-paren': ['error', {
+            anonymous: 'never',
+            named: 'never',
+            asyncArrow: 'always',
+        }],
+    },
+}, {
+    files: ['test/**/*.js', '**/*.mjs'],
+    languageOptions: {
+        sourceType: 'module'
+    }
+}];
