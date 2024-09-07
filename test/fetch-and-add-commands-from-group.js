e87a24ecbcc465266b22148f342bf213ee14a59f
Raymond Hammarling <raymond@hammarlings.se>
2024-09-07 05:46:27 +0200

    Change Next-Fetch-Date from header to part of body

--- test/fetch-and-add-commands-from-group.js
+++ test/fetch-and-add-commands-from-group.js
@@ -14,10 +14,9 @@ test('works', async t => {
     const { get, store } = t.context;
 
     get.resolves({
-        headers: new Headers({
-            'Next-Fetch-Date': '2024-05-09T01:02:03.000Z'
-        }),
+        headers: new Headers(),
         body: {
+            nextFetchDate: '2024-05-09T01:02:03.000Z',
             commands: [
                 { name: 'some-command', url: '/some-command' },
                 { name: 'another-command', url: '/another-command' },
@@ -52,10 +51,9 @@ test('throws error on malformed GET response', async t => {
     const { get, store } = t.context;
 
     get.resolves({
-        headers: new Headers({
-            'Next-Fetch-Date': '2024-05-09T01:02:03.000Z'
-        }),
+        headers: new Headers(),
         body: {
+            nextFetchDate: 'wowie',
             someRandomStuff: 'cool'
         }
     });
@@ -70,30 +68,4 @@ test('throws error on malformed GET response', async t => {
     const err = await t.throwsAsync(() => fetchAndAddCommandsFromGroup({ get, store }, groupId), { any: true });
     t.true(err.message.startsWith(`When running fetchAndAddCommandsFromGroup for group ${groupId}:`));
     t.snapshot(err.cause.errors);
-});
-
-test('throws error on malformed date in Next-Fetch-Date header', async t => {
-    const { get, store } = t.context;
-
-    get.resolves({
-        headers: new Headers({
-            'Next-Fetch-Date': 'something random'
-        }),
-        body: {
-            commands: [
-                { name: 'some-command', url: '/some-command' },
-                { name: 'another-command', url: '/another-command' },
-            ]
-        }
-    });
-
-    await store.set('command-groups', {
-        [groupId]: {
-            url: 'https://example.com/.commands',
-            commands: []
-        }
-    });
-
-    const err = await t.throwsAsync(() => fetchAndAddCommandsFromGroup({ get, store }, groupId));
-    t.is(err.message, `When running fetchAndAddCommandsFromGroup for group ${groupId}: Unable to parse date from header Next-Fetch-Date`);
 });
